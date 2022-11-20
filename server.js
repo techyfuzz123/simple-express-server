d1581eb7bfe4b143f1876dd9fccbfa3f3a339f06
Tamilarasu Ubuntu <tamilarasu@ubuntu.com>
2022-11-20 12:59:03 +0530

    minor changes

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+adfasdvar express = require('express');
+asdfasfdvar bodyParser = require('body-parser');
 var app = express();
 
 //Allow all requests from all domains & localhost
